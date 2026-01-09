7b9aca0277109431fdecda60fcd137b2dd24d80c
kush.gupta <kush.gupta@samsung.com>
2026-01-09 08:16:17 +0000

    Initial commit

---
+++ numbers_variables.py
@@ -0,0 +1,24 @@
+x = 9
+y = 5
+# Arithmetic operators
+print(f"division: {x/y}")
+print(f"multiply: {x*y}")
+print(f"subtract: {x-y}")
+print(f"modulo: {x%y}")
+print(f"power: {y**3}")
+print(f"2+10*10+3: {2+10*10+3}")
+print(f"(2+10)*(10+3): {(2+10)*(10+3)}")
+
+x=10
+print(f"now x is {x}")
+print(f"x+x: {x+x}")
+print(f"x: {x}")
+print(f"type(x): {type(x)}")
+x = 10.5
+print(f"now x is {x}")
+print(f"type(x): {type(x)}")
+
+# it is allowed to use variable names as built-in type names, but not recommended
+str = 10
+print(f"str is {str}")
+print(f"type(str): {type(str)}")
