e0f9c927f5ff5212952e81bff925e460fc336643
kush.gupta <kushguptacse@gmail.com>
2026-01-14 10:41:50 +0000

    Add additional boolean operation examples to bools.py and README

--- bools.py
+++ bools.py
@@ -6,3 +6,15 @@ print(type(val)) # print <class 'bool'>
 val = None
 print(val) # print None
 print(type(val)) # print <class 'NoneType'>
+
+print(f"'2'==2: {'2'==2}") # print False
+print(f"2==2.0: {2==2.0}") # print True as for float python just check value.
+print(f"2!=3: {2!=3}") # print True
+print(f"2!=2.0: {2!=2.0}") # print False
+print(f"1<2<3: {1<2<3}")# print True. it is evaluated left to right
+print(f"1<2>3: {1<2>3}")# print False. it is evaluated left to right
+
+print(f"1<2 and 2<3: {1<2 and 2<3}")# print True.
+print(f"1<2 or 2<3: {1<2 or 2>3}")# print True.
+print(f"not 1==1: {not 1==1}")# print False.
+print(f"1!=1: {1!=1}")# print False.
