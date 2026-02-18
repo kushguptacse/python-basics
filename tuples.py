cc5238759b818f52bc43b3e2c5713b6314353465
Kush Gupta <kushguptacse@gmail.com>
2026-02-18 04:19:51 +0000

    tuple un-packing example added

--- tuples.py
+++ tuples.py
@@ -1,11 +1,15 @@
-sample = (1,2,3)
-print(sample) # print (1,2,3)
-print(sample[0]) # print 1
-print(sample[:2]) # print (1,2)
-sample = ('a','a','b')
-print(len(sample)) # print 3
-print(sample.count('a')) # print 2
-print(sample.index('a')) # print 0
-sample = ('k',[1,2],9.1) # valid, tuple allows dict and all type as value
+sample = (1, 2, 3)
+print(sample)  # print (1,2,3)
+print(sample[0])  # print 1
+print(sample[:2])  # print (1,2)
+sample = ("a", "a", "b")
+print(len(sample))  # print 3
+print(sample.count("a"))  # print 2
+print(sample.index("a"))  # print 0
+sample = ("k", [1, 2], 9.1)  # valid, tuple allows dict and all type as value
 sample[1][1] = 44
-print(sample) # print ('k', [1, 44], 9.1)
+print(sample)  # print ('k', [1, 44], 9.1)
+
+tuple_var = (1, 2, 3, 4)
+a, _, b, d = tuple_var #tuple un-packing
+print(_) # 2
